090ec8e8b4352ea0045fb4b3b34cb53ab57fe84e
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-03-25 04:33:34 +0530

    refactor(cli): consolidate misc wrappers and rename shortcuts
    Move n8n wrapper from n8n.nix into misc.nix alongside other tool
    wrappers. Enable qwen-code as `qwen` and shorten kilocode to `kilo`.

--- config/repl/misc.nix
+++ config/repl/misc.nix
@@ -5,11 +5,11 @@
 }: {
   home.packages = lib.attrsets.attrValues {
     ## Qwen Code
-    # qwen-code = pkgs.writeShellScriptBin "qwen-code" ''
-    #   exec ${pkgs.pnpm}/bin/pnpm dlx @qwen-code/qwen-code@latest "$@"
-    # '';
+    qwen-code = pkgs.writeShellScriptBin "qwen" ''
+      exec ${pkgs.pnpm}/bin/pnpm dlx @qwen-code/qwen-code@latest "$@"
+    '';
     ## KiloCode
-    kilocode-cli = pkgs.writeShellScriptBin "kilo-code" ''
+    kilocode-cli = pkgs.writeShellScriptBin "kilo" ''
       exec ${pkgs.pnpm}/bin/pnpm --package=@kilocode/cli dlx kilocode "$@"
     '';
   };
