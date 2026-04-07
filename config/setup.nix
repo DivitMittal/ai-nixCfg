06a3a2c5039a09c95993cdc45bddc303ffa5707e
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-04-08 02:40:50 +0530

    refactor(config): reorganize into home/ subdir and rename common to _common
    Move all home-manager configs under config/home/ and rename
    config/repl/common/ to config/home/repl/_common/ to better reflect
    their scope. Simplifies setup.nix to use import-tree ./home directly.

--- config/setup.nix
+++ config/setup.nix
@@ -1,23 +1,9 @@
-self: let
-  ## Eagerly resolve config files at flake-parts level (shallow call stack) to avoid
-  ## hitting Nix's max-call-depth when lib.filesystem.listFilesRecursive is called
-  ## deep inside the home-manager module system evaluation.
-  configFiles =
-    (
-      self.inputs."import-tree"
-        .withLib self.inputs.nixpkgs.lib
-        .matchNot "setup\\.nix|.*(default|lib)\\.nix"
-        .pipeTo (x: x)
-    )
-    ./.;
-in
-{
-  imports =
-    configFiles
-    ++ [
-      self.homeManagerModules.default
-      self.inputs.nix-openclaw.homeManagerModules.openclaw
-    ];
+self: {
+  imports = [
+    (self.inputs.import-tree ./home)
+    self.homeManagerModules.default
+    self.inputs.nix-openclaw.homeManagerModules.openclaw
+  ];
 
   _module.args = {
     ai-nixCfg = self;
