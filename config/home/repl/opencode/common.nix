2c6dd7df271204ea586c45f922fe9788e972cec0
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-06-09 21:31:10 +0530

    refactor(repl/opencode): remove opencode repl config migrated to coding/opencode

--- config/home/repl/opencode/common.nix
+++
@@ -1,9 +0,0 @@
-{lib, ...}: let
-  common = import ../_common {inherit lib;};
-  inherit (common.lib) memoryInstruction;
-  inherit (common.rules) combinedRules;
-in {
-  programs.opencode = {
-    context = memoryInstruction + "\n\n" + combinedRules;
-  };
-}
