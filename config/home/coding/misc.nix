4b6cc78080fd445f63bab79de9c5c98df585ff8e
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-06-09 21:31:34 +0530

    feat(coding): add misc coding tools config
    Migrated from config/home/repl/misc.nix.

---
+++ config/home/coding/misc.nix
@@ -0,0 +1,13 @@
+{
+  lib,
+  pkgs,
+  customLib,
+  ...
+}: {
+  home.packages = lib.attrsets.attrValues {
+    ## Qwen Code
+    qwen-code = customLib.mkPnpmDlxBin pkgs "qwen" "@qwen-code/qwen-code@latest";
+    ## KiloCode
+    kilocode-cli = customLib.mkPnpmDlxBin pkgs "kilo" "--package=@kilocode/cli kilocode";
+  };
+}
