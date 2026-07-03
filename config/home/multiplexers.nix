51a2130c78343fa7a74765c32da52d1f4914c371
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-07-03 10:28:26 +0530

    feat(herdr): add pane and notification settings

--- config/home/multiplexers.nix
+++ config/home/multiplexers.nix
@@ -6,8 +6,12 @@
   programs.herdr = {
     enable = true;
     settings = {
+      keys.prefix = "ctrl+a";
       theme.name = "terminal";
       terminal.shell_mode = "auto";
+      ui.show_agent_labels_on_pane_borders = true;
+      ui.sound.enabled = true;
+      ui.toast.delivery = "herdr";
     };
   };
 
